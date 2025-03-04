6148c08cadb5669ba7d131e0be86569a58faf563
ANEJDAM Ayoub <Ayoub.ANEJDAM@akkodis.com>
2025-03-04 10:34:30 +0100

    add some changes to UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
@@ -40,5 +41,48 @@ final class UserController extends AbstractController
             'user' => $user,
             'roles' => $roles,
         ]);
+    }
+
+    #[Route('/api/users', name: 'app_user_api', methods: ['GET'])]
+    public function listUsers(UserRepository $userRepository): JsonResponse
+    {   
+        $users = $userRepository->findAll();
+        $data=[];
+        foreach ($users as $user) {
+            $data[] = [
+                'id' => $user->getId(),
+                'Last_Name' => $user->getNom(),
+                'First_Name' => $user->getPrenom(),
+                'email' => $user->getEmail(),
+                'adresse' => $user->getAdresse(),
+                'roles' => $user->getRoles(),
+            ];
+        }
+        return $this->json($data);
+    }
+
+    #[Route('/api/user/{id}', name: 'app_user_api_id', methods: ['PUT'])]
+    public function modifUser(int $id, UserRepository $userRepository, EntityManagerInterface $entityManager, Request $request): JsonResponse
+    {   
+        $user = $userRepository->find($id);
+        if(!$user){
+            return $this->json(['message' => 'User not found'], 404);
+        }
+        $data= json_decode($request->getContent(), true);
+        if(isset($data['nom'])){
+            $user->setNom($data['nom']);
+        }
+        if(isset($data['prenom'])){
+            $user->setPrenom($data['prenom']);
+        }
+        if(isset($data['adresse'])){
+            $user->setAdresse($data['adresse']);
+        }
+        if(isset($data['roles'])){
+            $user->setRoles($data['roles']);
+        }
+        $entityManager->flush();
+
+        return $this->json(['message' => 'User updated']);
     }
 }
